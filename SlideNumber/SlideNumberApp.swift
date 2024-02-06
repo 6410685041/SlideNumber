6dc105f3d35ba714444c56bb2744540393d30db2
6410685041 <6410685041@student.tu.ac.th>
2024-02-06 10:24:07 +0700

    Initial Commit

---
+++ SlideNumber/SlideNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideNumberApp.swift
+//  SlideNumber
+//
+//  Created by นางสาวชลิศา ธรรมราช on 6/2/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct SlideNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
